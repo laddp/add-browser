a9b8500b4ed6ffbb363fa92ed949e41bbdf7d6a5
pmladd <pmladd@gmail.com>
2016-03-16 15:04:53 -0400

    Remove spurious main method from BatchQueryDialog

--- src/net/pladd/ADDBrowser/BatchQueryDialog.java
+++ src/net/pladd/ADDBrowser/BatchQueryDialog.java
@@ -36,19 +36,6 @@ public class BatchQueryDialog extends JDialog {
 
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		try {
-			BatchQueryDialog dialog = new BatchQueryDialog();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
 	 * Create the dialog.
 	 */
 	public BatchQueryDialog() {
